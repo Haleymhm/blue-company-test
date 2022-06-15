f362faec77a7b7ad077f37df5b9af7d27266bc97
Haleym Hidalgo <haleymhm@gmail.com>
2022-06-15 04:17:20 -0400

    Factories y Seeders

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,18 +3,19 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
 
 class ProductFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+    protected $model = Product::class;
+
     public function definition()
     {
         return [
-            //
+            'nombre_prod' => $this->faker->sentence,
+            'valor' => rand(7,9),
+            'fecha_expiracion' => now(),
+            'category_id' => rand(1,9)
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Product;
+use App\Models\Category;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,6 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory(1)->create();
+        Category::factory(11)->create();
+        Product::factory(100)->create();
 
     }
 }
